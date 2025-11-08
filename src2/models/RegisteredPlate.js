1922e652e301f11633bb40a7ef9594c2915256d8
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2025-11-08 15:56:13 -0500

    Delete src2 directory

--- src2/models/RegisteredPlate.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-//to be changed depending on schema
-const RegisteredPlateSchema = new mongoose.Schema({
-  plateNumber: { type: String, unique: true, required: true },
-  type: { type: String, enum: ["STUDENT", "FACULTY", "GENERAL"], default: "GENERAL" }
-});
-
-export default mongoose.models.RegisteredPlate || mongoose.model("RegisteredPlate", RegisteredPlateSchema);
